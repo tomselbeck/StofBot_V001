0a28dfae10e068bcf69f4c4459351d86d6332efd
tomselbeck <tomselbeck@gmail.com>
2015-11-14 23:47:50 +0100

    Serial Communication

--- Arduino/T_ArduinoSide/T_ArduinoSide.ino
+++ Arduino/T_ArduinoSide/T_ArduinoSide.ino
@@ -1,17 +1,16 @@
 
-// Test
+char inMessage [8];
+int packetSwitch = 0;
+int packetCount = 0;
+int receivePin = 13;
+
 void setup() {
   // initialize serial communication at 9600 bits per second:
 
     Serial.begin(9600);
-    pinMode(13, OUTPUT);
+    pinMode(receivePin, OUTPUT);
 
-    // Blink light to verify it works 
-    Serial.println ("Blinking pin 13");
-    digitalWrite (13, HIGH);
-    delay(500);
-    digitalWrite(13,LOW);
-    delay(500);
+
 
 
     Serial.print ("Arduino is ready to talk");
@@ -21,36 +20,58 @@ void setup() {
 
 void loop() {
 
+// Check incomming Serial data // 
+serialIn();
+
+
+
+}
+
+
+
+int serialIn(){
 
 // Check for serial input // 
-  
   if(Serial.available() > 0) {
-        int data = Serial.read();
-        Serial.println("");
-        Serial.print ("Received packet:");
+        char data = Serial.read();
+        //Serial.println("");
+        //Serial.print ("Received a Byte:");
+
+        // Convert data to Asci caracter 
         
-        char datab = data;
-        Serial.print (datab);
+        char dataAsci = data;
+        //Serial.print (dataAsci);
 
+        // Check for opening byte 
 
-
-        // Do stuff based on the input command // 
-
-        // Construct incomming packet // 
-
-
-
-        
-      
-        if (datab == '1' ){
-         Serial.println("");
-         Serial.print("Blinking lights!");
-         digitalWrite (13, HIGH);
-         delay(1000);
-         digitalWrite(13,LOW);
-         delay(1000);
+        if (dataAsci ='O'){
+          packetSwitch = 1;
+          char Inmessage [8];
+         // Serial.println("");
+          //Serial.print("Started receiving packet");
+          digitalWrite (receivePin, HIGH); 
+        }
+         // Check for closing byte or if the packet is too long, then print the fineshed message
+        if (dataAsci = 'C' || packetCount>8){
+          
+          packetSwitch = 0;
+          packetCount = 0;
+         // Serial.println("Packet received");
+          //digitalWrite (receivePin, LOW);
+         // Serial.print (inMessage);
+          // Print finished packet //           
         }
     
+
+       // If the packet is being received, start logging bytes
+
+       if (packetSwitch == 1){
+          inMessage[packetCount] = dataAsci;
+         // Serial.print (" PacketCount:");
+         // Serial.print(packetCount);          
+          packetCount++;         
+       }    
   }
+
 }
 
