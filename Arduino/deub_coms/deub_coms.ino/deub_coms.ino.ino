461f7f6201dbc10281c8f385c5c67f3606e93d47
tomselbeck <tomselbeck@gmail.com>
2015-11-26 20:54:03 +0100

    Comsjizzle

---
+++ Arduino/deub_coms/deub_coms.ino/deub_coms.ino.ino
@@ -0,0 +1,22 @@
+int serialdata = 0;
+int inbyte = 0;
+int slashtest = false;
+int count = 0;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(13,OUTPUT);
+  digitalWrite (13,LOW);
+  
+}
+
+void loop() {
+
+
+Serial.println (1);
+delay(500);
+  
+}
+
+
+
